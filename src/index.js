f61f70ff3d609b4159760980be70682f1abca6f2
= <=>
2021-01-24 20:18:20 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -5,13 +5,10 @@ import App from './App'
 import manageUsers from './reducers/manageUsers'
 
 
-// add imports and code
-
-
+let store = createStore(manageUsers)
 ReactDOM.render(
-  // add imports and code
-  <App />
-  // add imports and code
-  ,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
