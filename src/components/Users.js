601877da8ea087a9d2de6d6773df190ed85c4fd3
Gabriela Johnson <johnsongabriela12@gmail.com>
2021-02-06 19:42:01 -0800

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux'
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Users extends Component {
-
   render() {
-    // debugger
+    let users = this.props.users.map((user, index) => (
+      <li key={index}>{user.username}</li>
+    ));
+
     return (
       <div>
-        <ul>
-          Users!
-          {console.log(this.props.users)}
-          {console.log(this.props.userCount)}
-          {this.props.users.map(u => <li>{u.username}</li>)} <br/>
-          Count: {this.props.userCount}
-        </ul>
+        {this.props.numberOfUsers}
+        <ul>{users}</ul>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => { 
-  return {users: state.users, userCount: state.users.length}
-}
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+    numberOfUsers: state.users.length,
+  };
+};
 
-// connect this component to Redux
-export default connect(mapStateToProps)(Users)
+export default connect(mapStateToProps)(Users);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import manageUsers from "./reducers/manageUsers";
 
-import manageUsers from './reducers/manageUsers'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-
-const store = createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
+let store = createStore(
+  manageUsers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  < Provider store = { store } >
-  <App />
-  </Provider>
-  ,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
